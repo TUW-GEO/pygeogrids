0d4c519131067b20294da03b3d64c059971c2ad1
Sebastian Hahn <sebastian.hahn@geo.tuwien.ac.at>
2015-09-29 13:15:36 +0200

    update file header and module docstring

--- pygeogrids/grids.py
+++ pygeogrids/grids.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2013,Vienna University of Technology, Department of Geodesy and Geoinformation
+# Copyright (c) 2015, Vienna University of Technology,
+# Department of Geodesy and Geoinformation
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
@@ -8,14 +9,15 @@
 #    * Redistributions in binary form must reproduce the above copyright
 #      notice, this list of conditions and the following disclaimer in the
 #      documentation and/or other materials provided with the distribution.
-#    * Neither the name of the Vienna University of Technology, Department of Geodesy and Geoinformation nor the
-#      names of its contributors may be used to endorse or promote products
-#      derived from this software without specific prior written permission.
+#    * Neither the name of the Vienna University of Technology, Department
+#      of Geodesy and Geoinformation nor the names of its contributors may
+#      be used to endorse or promote products derived from this software
+#      without specific prior written permission.
 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL VIENNA UNIVERSITY OF TECHNOLOGY,
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL VIENNA UNIVERSITY OF TECHNOLOGY,
 # DEPARTMENT OF GEODESY AND GEOINFORMATION BE LIABLE FOR ANY
 # DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
@@ -24,20 +26,22 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-'''
-Created on Aug 26, 2013
+# created on Aug 26, 2013
+# author: Christoph Paulik Christoph.Paulik@geo.tuwien.ac.at
 
-@author: Christoph Paulik Christoph.Paulik@geo.tuwien.ac.at
-'''
-
-import pygeogrids.nearest_neighbor as NN
+"""
+The grids module defines the grid classes.
+"""
 
 import numpy as np
 import numpy.testing as nptest
+
 try:
     from itertools import izip as zip
 except ImportError:
     pass  # python3
+
+import pygeogrids.nearest_neighbor as NN
 
 
 class GridDefinitionError(Exception):
