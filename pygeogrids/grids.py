Fix lookup table calculation in case of unsorted subset order.

pygeogrids/grids.py
@@ -517,7 +517,7 @@ class BasicGrid(object):
             if not self.allpoints:
                 gpi_lut = np.empty_like(self.gpis)
                 gpi_lut.fill(-1)
-                gpi_lut[self.subset] = active_lut
+                gpi_lut[self.gpis[self.subset]] = active_lut
             elif not self.gpidirect:
                 gpi_lut = np.empty(np.max(self.activegpis) + 1, dtype=np.int64)
                 gpi_lut.fill(-1)